306a696acbfa07ec0efc8946e6f45a5beb239f8a
xiaohua <1396951044@qq.com>
2020-01-09 09:43:19 +0800

    --

--- videoproject/settings.py
+++ videoproject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wg*kgsb5$ok23k3t%g)^2mf6++v(o(j1d%-vfd0k(@f(@jg(qh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
